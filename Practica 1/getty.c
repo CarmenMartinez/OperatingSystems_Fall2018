8adcf9a8e51037ca4bb6aa8c1f047cb8f982d6a7
Carmen Martinez <carmen.mtzn@gmail.com>
2018-09-03 18:03:36 -0500

    init file

---
+++ Practica 1/getty.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int x;
+	printf("Hola mundo\n");
+	scanf("%d", &x);
+	return 0;
+}
---
+++ Practica 1/init.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<signal.h>
+
+#define NPROCESS 6
+
+char* gettyParam[] = {"-e", "./getty", NULL};
+
+void handleChild();
+
+int main(){
+	pid_t p;
+	int status, i;
+	char command[50];
+	char pid[4];
+
+
+	for(i = 0; i < NPROCESS; i++){
+		p = fork();
+		if(p == 0){
+			//Creacion de 6 procesos 
+			execvp("xterm", gettyParam);
+		}
+		
+	}
+
+	signal(SIGCHLD, handleChild);			
+
+	while(1){
+		fgets(command, 50, stdin);	
+		if(strncmp(command, "shutdown", 8) == 0){
+			//0 = grupo de procesos
+			//-PID = grupo de procesos absoluto
+			kill(0, SIGTERM);
+		}
+	}		
+}
+
+void handleChild() {
+	int status;
+	wait(NULL);
+	pid_t p;
+	p = fork();
+	if(p == 0){
+		//Creacion de 6 procesos 
+		execvp("xterm", gettyParam);
+	}
+}
