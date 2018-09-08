dc4bc20df3741f3319914052a578786403f026a7
XUbuntu <sierravgmr@gmail.com>
2018-09-08 10:51:22 -0600

    Calculate PI/4 F

---
+++ Practica 3/pi.c
@@ -0,0 +1,35 @@
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/time.h>
+
+#define ITERATIONS 2000000000
+
+int main(){
+	long double pi = 0, x;
+	int i = 0;
+	struct timeval ts;
+	long long stop_ts;
+	long long start_ts;
+	int elapsed_time;
+	
+	gettimeofday(&ts, NULL);
+	start_ts = ts.tv_sec * 1000000 + ts.tv_usec;
+	
+	
+	for(i = 0; i < ITERATIONS; i++){
+		x = pow(-1, i);
+		x /= (2*i)+1;
+		pi += x;
+		x = 0;
+	}
+	
+	gettimeofday(&ts, NULL);
+	stop_ts = ts.tv_sec * 1000000 + ts.tv_usec;
+	
+	elapsed_time = (int) (stop_ts - start_ts);
+	
+	printf("The value of pi/4 is %Lf\n", pi);
+	printf("And it took %d microseconds\n", elapsed_time);
+	return 0;
+}
