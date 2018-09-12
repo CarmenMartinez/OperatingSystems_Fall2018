a6c925173844f5235016ccfd6bf081ac6122e746
XUbuntu <sierravgmr@gmail.com>
2018-09-12 15:19:30 -0600

    Practica 3 DONE and FUNCTIONAL

--- Practica 3/fork.c
+++ Practica 3/fork.c
@@ -9,38 +9,37 @@
 #include <signal.h>
 #include <sched.h>
 #include <sys/time.h>
+#include <sys/shm.h>
 
 #define ITERATIONS 2000000000
 #define NPROCESS 4
-
-int activeSons = 0;
-
-//time managment
-struct timeval ts;
-long long stop_ts;
-long long start_ts;
-int elapsed_time;
-
-void handleChild();
 
 int main(){
 	int i = 0, high, low, j, memory;
 	pid_t p;
 	long double pi = 0, x;
+	int status;
+	long double res_pi = 0;
 	//key_t key;
+	
+	//time managment
+	struct timeval ts;
+	long long stop_ts;
+	long long start_ts;
+	int elapsed_time;
 	
 	gettimeofday(&ts, NULL);
 	start_ts = ts.tv_sec * 1000000 + ts.tv_usec;
 	
 	//crear memoria
-	memory = shmget(IPC_PRIVATE, sizeof(long double), IPC_CREAT | IPC_EXCL);
-	printf("Memory id = %d\n", memory);
+	memory = shmget(IPC_PRIVATE, sizeof(long double)*NPROCESS, IPC_CREAT | IPC_EXCL | 0666);
+	long double * res;
+	res = (long double*) shmat(memory, NULL, 0);
 	for(j = 0; j < NPROCESS; j++){
 		high = (ITERATIONS / 4) * (j+1);
 		low = (ITERATIONS / 4) * (j);
 		p = fork();
 		if (p == 0){
-			activeSons++;
 			pi = 0;
 			for(i = low; i < high; i++){
 				x = pow(-1, i);
@@ -48,32 +47,25 @@ int main(){
 				pi += x;
 				x = 0;
 			}
-			printf("The value of %d pi/4 is %0.19Lf\n", j, pi);
-			//pedir memoria
-			//guardar en memoria
+			res[j] = pi;
 			exit(0);
 		}
 	}
 	
-	signal(SIGCHLD, handleChild);	
-	
-	return 0;
-}
-
-void handleChild() {
-	int status;
-	wait(&status);
-	
-	/*if(activeSons > 0){
-		activeSons--;
-		return;
-	}*/
-	
+	for(j = 0; j < NPROCESS; j++){
+		wait(&status);
+	}
+	for(j = 0; j < NPROCESS; j++){
+		res_pi += res[j];
+	}
 	gettimeofday(&ts, NULL);
 	stop_ts = ts.tv_sec * 1000000 + ts.tv_usec;
 	
 	elapsed_time = (int) (stop_ts - start_ts);
 	
-	//printf("The value of pi/4 is %Lf\n", pi);
+	printf("The value of pi/4 is %0.19Lf\n", res_pi);
 	printf("And it took %d microseconds\n", elapsed_time);
+	
+	return 0;
 }
+
