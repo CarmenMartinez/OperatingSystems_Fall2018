cafab15f0c89d18c1116158e044bef2c86487f4d
XUbuntu <sierravgmr@gmail.com>
2018-09-21 17:42:59 -0600

    RR - RR2 F

---
+++ Practica 4/ULTs/todo/schedulerRR2.c
@@ -0,0 +1,76 @@
+#include <scheduler.h>
+
+extern THANDLER threads[MAXTHREAD];
+extern int currthread;
+extern int blockevent;
+extern int unblockevent;
+
+QUEUE ready;
+QUEUE waitinginevent[MAXTHREAD];
+
+int quantum = 0;
+
+void scheduler(int arguments)
+{
+	int old,next;
+	int changethread=0;
+	int waitingthread=0;
+	
+	int event=arguments & 0xFF00;
+	int callingthread=arguments & 0xFF;
+	
+	
+
+	if(event==TIMER){
+		quantum++;
+		if(quantum == 2){
+			if(_emptyq(&ready) == 0){
+				threads[callingthread].status = READY;
+				_enqueue(&ready, callingthread);
+		
+				changethread=1;
+				
+			}
+			quantum = 0;
+		}
+	}
+	
+	if(event==NEWTHREAD)
+	{
+		// Un nuevo hilo va a la cola de listos
+		threads[callingthread].status=READY;
+		_enqueue(&ready,callingthread);
+	}
+	
+	if(event==BLOCKTHREAD)
+	{
+
+		threads[callingthread].status=BLOCKED;
+		_enqueue(&waitinginevent[blockevent],callingthread);
+
+		changethread=1;
+	}
+
+	if(event==ENDTHREAD)
+	{
+		threads[callingthread].status=END;
+		changethread=1;
+	}
+	
+	if(event==UNBLOCKTHREAD)
+	{
+			threads[callingthread].status=READY;
+			_enqueue(&ready,callingthread);
+	}
+
+	
+	if(changethread)
+	{
+		old=currthread;
+		next=_dequeue(&ready);
+		
+		threads[next].status=RUNNING;
+		_swapthreads(old,next);
+	}
+
+}
